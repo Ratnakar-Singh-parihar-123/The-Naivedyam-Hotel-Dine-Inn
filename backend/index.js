daa4bb6733e89532ffc21b632492783ab3b913f7
Babar Ali <babardev167@gmail.com>
2025-10-31 17:25:54 -0700

    url changed to vercel urls

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ connectCloudinary();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://resturant-app-peach.vercel.app",
     credentials: true,
   })
 );
